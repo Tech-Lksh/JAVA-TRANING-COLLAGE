90234606c3f2cd2a8df8c7f7a9ea397b96da575e
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2025-04-08 12:43:13 +0530

    TypeCasting

---
+++ HelloWorld.java
@@ -0,0 +1,31 @@
+// 1 problem
+public class HelloWorld {
+    public static void main(String[] args) {
+        // first
+        // System.out.println("Hello World"); // hello world
+
+        // second 
+        // int a = 'A';
+        // System.out.println(a); // 65
+
+        int a = 5;
+        int b = 10;
+
+        a =a+b;
+        b=a-b;
+        a=a-b;
+        // int c;
+        // c=a;
+        // a=b;
+        // b=c;
+         System.out.println("" +a+b);
+
+//         int x = 5;
+//  double y = 10;
+//  System.out.println(x + y);
+
+
+    }
+}
+
+
---
+++ TypeCasting.java
@@ -0,0 +1,34 @@
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        // Type casting 
+
+        byte a = 10;--     
+        short b = a;
+        int c = a;
+        double d = a;
+        System.out.println("Byte to short, int, double :- "+" b:- "+b+ " c:- " +c+ " d:- " +d);
+
+        short e = 10;
+        int f = e;
+        double g = e;
+        System.out.println("short to int, double :- "+" f:- "+f+ " g:- " +g);
+
+        int h = 10;
+        double i = h;
+        System.out.println("int to double :- " +" i:- "+i);
+
+        short j = 10;
+        byte k = (byte)j;
+        System.out.println("short to byte :- " +" k:- "+k);
+
+        int l = 10;
+        byte m = (byte)l;
+        System.out.println("int to byte :- " +" m:- "+m);
+
+        double n = 10;
+        byte o = (byte)n;
+        System.out.println("double to byte :- " +" o:- "+o);
+
+    }
+}
